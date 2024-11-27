bda6829b5573a1a493e66f2aaef0b59ecf623c35
codeStackr007 <prosperjohn143@gmail.com>
2024-11-27 02:24:36 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,82 @@
+//Text Replacement Effect
+const checkReplace = document.querySelector('.replace-me');
+
+if (checkReplace !== null) {
+  const replace = new ReplaceMe(checkReplace, {
+    animation: 'animated fadeIn',
+    speed: 2000,
+    separator: ',',
+    loopCount: 'infinite',
+    autoRun: true,
+  });
+}
+
+// User Scroll For Navbar
+function userScroll() {
+  const navbar = document.querySelector('.navbar');
+
+  window.addEventListener('scroll', () => {
+    if (window.scrollY > 50) {
+      navbar.classList.add('bg-dark');
+      navbar.classList.add('border-bottom');
+      navbar.classList.add('border-secondary');
+      navbar.classList.add('navbar-sticky');
+    } else {
+      navbar.classList.remove('bg-dark');
+      navbar.classList.remove('border-bottom');
+      navbar.classList.remove('border-secondary');
+      navbar.classList.remove('navbar-sticky');
+    }
+  });
+}
+
+document.addEventListener('DOMContentLoaded', userScroll);
+
+// Video Modal
+const videoBtn = document.querySelector('.video-btn');
+const videoModal = document.querySelector('#videoModal');
+const video = document.querySelector('#video');
+let videoSrc;
+
+if (videoBtn !== null) {
+  videoBtn.addEventListener('click', () => {
+    videoSrc = videoBtn.getAttribute('data-bs-src');
+  });
+}
+
+if (videoModal !== null) {
+  videoModal.addEventListener('shown.bs.modal', () => {
+    video.setAttribute(
+      'src',
+      videoSrc + '?autoplay=1;modestbranding=1;showInfo=0'
+    );
+  });
+
+  videoModal.addEventListener('hide.bs.modal', () => {
+    video.setAttribute('src', videoSrc);
+  });
+}
+
+// Smooth Scroll to Top Button
+document.addEventListener("DOMContentLoaded", () => {
+  const toTopButton = document.getElementById("to-top");
+
+  // Show or hide the button based on scroll position
+  window.addEventListener("scroll", () => {
+    if (window.scrollY > 300) {
+      toTopButton.style.opacity = "1";
+      toTopButton.style.pointerEvents = "auto";
+    } else {
+      toTopButton.style.opacity = "0";
+      toTopButton.style.pointerEvents = "none";
+    }
+  });
+
+  // Smooth scroll to top when the button is clicked
+  toTopButton.addEventListener("click", () => {
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth",
+    });
+  });
+});
